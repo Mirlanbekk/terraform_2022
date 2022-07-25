d3f102261a784c10bab82bfcbd3bb69200bbd4a5
Mirlanbekk <pirmatov@gmail.com>
2022-07-25 12:40:14 -0700

    Creating EC2 using data source

---
+++ data_source/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+    filter {
+        name = "tag:Name"
+        values = ["default"]
+    }
+}
+
+
+
+
+resource "aws_subnet" "web" {
+    vpc_id = data.aws_vpc.default.id
+    cidr_block = "10.0.0.0/16"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.web.id
+
+
+  tags = {
+      Name = "EC2_Mir"
+  }
+} 
+
