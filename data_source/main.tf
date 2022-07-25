902f849d0caf2662174114e68a1e682baddeef00
Mirlanbekk <pirmatov@gmail.com>
2022-07-25 12:43:55 -0700

    Creating EC2 using data source

--- data_source/main.tf
+++ data_source/main.tf
@@ -9,15 +9,12 @@ data "aws_vpc" "default" {
     }
 }
 
-
-
-
 resource "aws_subnet" "web" {
     vpc_id = data.aws_vpc.default.id
     cidr_block = "10.0.0.0/16"
 }
 resource "aws_instance" "web" {
-  ami           = "ami-0cff7528ff583bf9a"
+  ami           = "ami id"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.web.id
 
